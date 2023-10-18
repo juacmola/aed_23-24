db63297a3b80a17416a4a5a9020fe0850b05c9fe
juacmola <148327149+juacmola@users.noreply.github.com>
2023-10-18 11:29:31 +0200

    Add files via upload
    Ejercicio 001 hecho

---
+++ 001.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void) {
+    int contador;
+    string palabra;
+    cout << contador << ". " << palabra << endl;
+    /*while (true) {
+        cout << contador << ". " << palabra << endl;
+    }*/
+}

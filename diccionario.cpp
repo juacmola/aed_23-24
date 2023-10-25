5b35744dd9abd1e56fb71208be0dd6837c9ee9eb
juacmola <148327149+juacmola@users.noreply.github.com>
2023-10-25 13:31:34 +0200

    Update diccionario.cpp
    He actualizado los métodos de clase con la info que nos dan en el pp

--- diccionario.cpp
+++ diccionario.cpp
@@ -6,17 +6,19 @@ DicPalabras::DicPalabras () {
     contador= 0;
 }
 
-void DicPalabras::vacia (void) {
+void DicPalabras::vaciar (void) {
     lista.clear();
     contador= 0;
 }
 
 void DicPalabras::inserta (string palabra){
-    
+    //Insertar palabra en lista de forma ordenada
+    //Si no estaba ya, contador++
 }
 
 bool DicPalabras::consulta (string palabra){
-    
+    //Buscar en la lista la palabra
+    //Devolver true/false si está o no, respectivamente
 }
 
 

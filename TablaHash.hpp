68c2bbf597091134e4445d1b3464a071b1c4045b
juacmola <148327149+juacmola@users.noreply.github.com>
2023-11-28 19:40:18 +0100

    Update TablaHash.hpp
    TablaHash final

--- TablaHash.hpp
+++ TablaHash.hpp
@@ -1,18 +1,31 @@
-#include "TablaHash.hpp"
+#ifndef __TABLA_HASH_H_INCLUDED
+#define __TABLA_HASH_H_INCLUDED
 
-#ifndef _DICCIONARIO_H_INCLUDED
-#define _DICCIONARIO_H_INCLUDED
+#include <list>
+#include <string>
 
 using namespace std;
 
-class DicPalabras {
-    private:
-        TablaHash tabla;
-    public:
-        DicPalabras ();
-        void vaciar () { tabla.vaciar(); }
-        void insertar (string palabra) { tabla.insertar(palabra); }
-        bool consultar (string palabra) { return tabla.consultar(palabra); }
-        int numElem (void) { return tabla.numElem(); }
+class TablaHash {
+	private:
+        	list<string> *T;
+        	int B;
+		void reestructurar();
+        	int nElem;
+	public:
+        	TablaHash();
+        	~TablaHash();
+		unsigned long Hash(string palabra);
+		void insertar (string palabra);
+		bool consultar (string palabra);
+        	void vaciar (void);
+		int numElem (void) { return nElem; }
+		
+		//list<string> getTabla() { return *T; }
+		//list<string>::iterator begin() const { return T->begin(); }
+		//list<string>::iterator end() const { return T->end(); }
+		
+		list<string> anagramas(string palabra);
+		
 };
 #endif

6fdd908d386ad2ca84de9ff8f187e433773b49ec
juacmola <148327149+juacmola@users.noreply.github.com>
2023-11-28 19:43:58 +0100

    Update diccionario.hpp
    Se añade la función anagramas para la tabla hash

--- diccionario.hpp
+++ diccionario.hpp
@@ -13,6 +13,9 @@ class DicPalabras {
         void vaciar () { tabla.vaciar(); }
         void insertar (string palabra) { tabla.insertar(palabra); }
         bool consultar (string palabra) { return tabla.consultar(palabra); }
-        int numElem (void) { return tabla.numElem(); }
+        int numElem (void) { return tabla.numElem();
+        
+        list<string> anagramas(string palabra) { return tabla.anagramas(palabra); }
 };
 #endif
+
